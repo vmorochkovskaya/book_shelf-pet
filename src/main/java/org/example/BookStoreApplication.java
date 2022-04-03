249ae0ed43415a32e5fec2dbd6fce262b001e93d
Vasilisa_Marachkousk <vasilisademuanova@mail.ru>
2022-04-03 21:58:33 +0200

    add db ec2 host

--- src/main/java/org/example/BookStoreApplication.java
+++ src/main/java/org/example/BookStoreApplication.java
@@ -2,14 +2,20 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BookStoreApplication.class);
+    }
 
 }
